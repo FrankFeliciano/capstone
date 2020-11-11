44f3c3dc00657caec029399cab15ad7d0291567f
Frank Feliciano <fsquared66@gmail.com>
2020-11-10 19:22:24 -0600

    Features/Sub Pages Layouts, Navigation/Responsive Functionalities

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+export default () => `
+<header>
+<div class="headerBox">
+  <div class="imageProp01">
+    <div>  
+      <img src="./images/Garden_Image.jpg" alt="garden logo" width=100%/>
+    </div>
+    <div>
+      <img src="./images/Volunteer_Image.jpg" alt="volunteer logo" width=100%/>
+    </div>
+    <div>
+      <img src="./images/SignUp_Image.jpg" alt="sign-up logo" width=100%/>
+    </div>
+    <div>
+      <img src="./images/Garden_Image.jpg" alt="garden logo" width=100%/>
+    </div>
+  </div>  
+</div>
+</header>`;
